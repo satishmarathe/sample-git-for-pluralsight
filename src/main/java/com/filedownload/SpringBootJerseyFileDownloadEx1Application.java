b9d4d1ed0a0938b7b135ef451924536e6c578ec6
Satish.Marathe <marathesinpune@gmail.com>
2018-09-18 14:02:37 +0530

    first initial commit

---
+++ src/main/java/com/filedownload/SpringBootJerseyFileDownloadEx1Application.java
@@ -0,0 +1,12 @@
+package com.filedownload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJerseyFileDownloadEx1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJerseyFileDownloadEx1Application.class, args);
+	}
+}
